fb09a57b4725f0d8069dc0549bb3114520b9bac8
Ahmet ilhan <ahmetilhan.dev@gmail.com>
2022-09-25 15:02:54 +0300

    errorHandler of axios improved

---
+++ src/api/constants/error.constants.ts
@@ -0,0 +1,6 @@
+export default {
+  SERVER_ERR: "There was a problem on the server side",
+  GITHUB_SERVER_ERR: "There was a problem on the github server side",
+  SYNTAX_ERR: "Syntax error occurred",
+  UNKNOWN_ERR: "An unknown error has occurred",
+};
--- src/api/plugins/axios.plugin.ts
+++ src/api/plugins/axios.plugin.ts
@@ -1,7 +1,8 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosError, AxiosInstance, AxiosResponse } from "axios";
 import authConstants from "../constants/auth.constants";
 import axiosConstants from "../constants/axios.constants";
-
+const LOCAL_MACHINE_USER_AGENT =
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36";
 const baseHTTP: AxiosInstance = axios.create({
   baseURL: "https://api.github.com",
   responseType: "json",
@@ -10,11 +11,29 @@ const baseHTTP: AxiosInstance = axios.create({
   headers: {
     Authorization: `Bearer ${authConstants.TOKEN}`,
     "Content-Type": "application/json",
+    "User-Agent": LOCAL_MACHINE_USER_AGENT,
   },
   auth: {
     username: authConstants.USERNAME,
     password: authConstants.PASSWORD,
   },
 });
+
+baseHTTP.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    if (!error.response) {
+      if (error.request) {
+        console.log(error.request);
+        return;
+      }
+      console.log(error.message);
+      return;
+    }
+    console.error(error.response.data);
+  }
+);
 
 export default baseHTTP;
