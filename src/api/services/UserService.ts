01f8456bd300b704bd761a374925d76eb7fab685
Ahmet ilhan <ahmetilhan.dev@gmail.com>
2022-09-25 15:43:36 +0300

    removed test username

--- src/api/services/UserService.ts
+++ src/api/services/UserService.ts
@@ -2,8 +2,10 @@ import { AxiosResponse } from "axios";
 import baseHTTP from "../plugins/axios.plugin";
 
 class UserService {
-  public getUserByUsername = async (username: string): Promise<AxiosResponse> => {
-    return baseHTTP.get(`/users/${username}asd`);
+  public getUserByUsername = async (
+    username: string
+  ): Promise<AxiosResponse> => {
+    return baseHTTP.get(`/users/${username}`);
   };
 }
 
