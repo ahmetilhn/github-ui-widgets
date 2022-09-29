1ae764b8991913622db13b20a70c51b50ad2336d
Ahmet ilhan <ahmetilhan.dev@gmail.com>
2022-09-29 23:59:19 +0300

    testIgnore paths added to jest.config

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/__tests__/*.spec.ts"],
+  testPathIgnorePatterns: ["/node_modules/", "/demo/", "/dist/", "/src/"],
 };
