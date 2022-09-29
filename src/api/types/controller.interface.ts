7c33e2b593ab0219606067fbb17f273d2ebb90b4
Ahmet ilhan <ahmetilhan.dev@gmail.com>
2022-09-29 20:16:38 +0300

    crud property added to controller type

--- src/api/types/controller.interface.ts
+++ src/api/types/controller.interface.ts
@@ -1,5 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 
 export default interface IController {
-  get: (req: Request, res: Response, next: NextFunction) => Promise<boolean>;
+  get?: (req: Request, res: Response, next: NextFunction) => Promise<unknown>;
+  post?: (req: Request, res: Response, next: NextFunction) => Promise<unknown>;
+  delete?: (req: Request, res: Response, next: NextFunction) => Promise<unknown>;
+  update?: (req: Request, res: Response, next: NextFunction) => Promise<unknown>;
 }
