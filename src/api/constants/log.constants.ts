9f9bbe6c6bd4cb193083f2c5636f052f9f957e42
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-10-01 02:31:45 +0300

    log dir updated for deployment

--- src/api/constants/log.constants.ts
+++ src/api/constants/log.constants.ts
@@ -2,12 +2,18 @@ import LogTypes from "../types/log.enum";
 export default {
   CRITICAL: {
     name: LogTypes.CRITICAL,
-    dir: process.cwd() + "/src/api/logs/" + LogTypes.CRITICAL + "/",
+    dir:
+      process.env.NODE_ENV === "production"
+        ? process.cwd() + "/dist/logs" + LogTypes.CRITICAL + "/"
+        : process.cwd() + "/src/api/logs/" + LogTypes.CRITICAL + "/",
     level: LogTypes.CRITICAL,
   },
   BASIC: {
     name: LogTypes.BASIC,
-    dir: process.cwd() + "/src/api/logs/" + LogTypes.BASIC + "/",
+    dir:
+      process.env.NODE_ENV === "production"
+        ? process.cwd() + "/dist/logs" + LogTypes.BASIC + "/"
+        : process.cwd() + "/src/api/logs/" + LogTypes.BASIC + "/",
     level: LogTypes.BASIC,
   },
 };
