4e8d08a29930959df2579d3be1c1904631324696
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-10-02 17:06:23 +0300

    welcome route added

---
+++ src/api/constants/api.constants.ts
@@ -0,0 +1,3 @@
+export default {
+  domain: "https://github-ui-widgets.onrender.com/",
+};
--- src/api/constants/route.constants.ts
+++ src/api/constants/route.constants.ts
@@ -1,14 +1,23 @@
+import apiConstants from "./api.constants";
 export default {
   profile: {
     path: "/:username/profile",
     name: "profile",
+    description:
+      "Allows you to retrieve a user's information such as followers, following, public repositories, gists",
+    example: apiConstants.domain + "ahmetilhan24/" + "profile",
   },
   repositories: {
     path: "/:username/repositories",
     name: "repositories",
+    description: "Lets you get public repositories",
+    example: apiConstants.domain + "ahmetilhan24/" + "repositories",
   },
   commits: {
     path: "/:username/:repo/commits",
     name: "commits",
+    description: "Lets you get commits of public repositories",
+    example:
+      apiConstants.domain + "ahmetilhan24/" + "github-ui-widgets/" + "commits",
   },
 };
