26a016ee3a8e53d5a652290583b7fd0d23248afc
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-10-02 16:04:32 +0300

    base service added to commit service

--- src/api/services/commit.service.ts
+++ src/api/services/commit.service.ts
@@ -1,13 +1,17 @@
+import { Request, Response, NextFunction } from "express";
 import { AxiosResponse } from "axios";
 import baseHTTP from "../plugins/axios.plugin";
+import BaseService from "./base.service";
 
-class CommitService {
-  public getCommitsByRepo = async (
-    username: string,
-    repo: string
-  ): Promise<AxiosResponse> => {
-    return baseHTTP.get(`/repos/${username}/${repo}/commits`);
+class CommitService extends BaseService {
+  constructor(req: Request, res: Response, next: NextFunction) {
+    super(req, res, next);
+  }
+  public getCommitsByRepo = async (): Promise<AxiosResponse> => {
+    return baseHTTP.get(
+      `/repos/${this.req.params.username}/${this.req.params.repo}/commits`
+    );
   };
 }
 
-export default new CommitService();
+export default CommitService;
